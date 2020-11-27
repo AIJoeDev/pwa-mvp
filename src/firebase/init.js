09a26fd4d01c51ffa11d45be17850ec336186f50
Jeff <admin@jeffwilliams.dev>
2020-11-26 23:45:27 -0700

    saved init.js file... what a fool

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,14 @@ import 'firebase/auth'
 
 // The configuration below is not sensitive data. You can serenely add your config here
 const config = {
-  apiKey: 'AIzaSyCe6CSzOUs6_Ij_VHa6hancdEi6CNBtw7c',
-  authDomain: 'bento-starter.firebaseapp.com',
-  databaseURL: 'https://bento-starter.firebaseio.com',
-  projectId: 'bento-starter',
-  storageBucket: 'bento-starter.appspot.com',
-  messagingSenderId: '458464977217'
+  apiKey: 'AIzaSyC1MuNXQDeLeC3jP4YWzdvq-XvA3m4cpbQ',
+  authDomain: 'ai-joe-mvp.firebaseapp.com',
+  databaseURL: 'https://ai-joe-mvp.firebaseio.com',
+  projectId: 'ai-joe-mvp',
+  storageBucket: 'ai-joe-mvp.appspot.com',
+  messagingSenderId: '690110000312',
+  appId: '1:690110000312:web:de8298eaf6aa686e3e584d',
+  measurementId: 'G-0YX9FY87DQ'
 }
 
 firebase.initializeApp(config)
