8c6beda85238ad86b54f290e87d7845fda53a89e
Jeff <admin@jeffwilliams.dev>
2020-11-29 13:12:45 -0700

    emulator setup

--- src/firebase/authentication.js
+++ src/firebase/authentication.js
@@ -3,6 +3,8 @@ import { isNil } from 'lodash'
 
 import store from '@/store'
 
+firebase.auth().useEmulator('http://localhost:9099/')
+
 firebase.auth().onAuthStateChanged(firebaseUser => {
   const actionToDispatch = isNil(firebaseUser) ? 'logout' : 'login'
   store.dispatch(`authentication/${actionToDispatch}`, firebaseUser)
